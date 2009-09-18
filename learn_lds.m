768ab32a326feea3a0e86db896717b4ff095727b
leili <leili@03993aaa-8331-11dc-9d23-8cf24f0ad205>
2009-09-18 01:38:03 +0000

    partial still not working

---
+++ learn_lds.m
@@ -0,0 +1,92 @@
+function [model, LL] = learn_lds(x, varargin)
+% learning model parameters for Linear Dynamical Systems (LDS), also known
+% as Kalman Filters.
+% Linear Dynamical Systems are described by the following equations:
+% z_1 = mu0 + w_1
+% z_n = A z_{n-1} + w_n
+% x_n = C x_n + v_n
+% w_1 ~ N(0, Q0)
+% w_n ~ N(0, Q)
+% v_n ~ N(0, R)
+%
+% Args:
+%   x: M * N matrix, M is number of sequences, N is the time duration.
+%
+% Optional Args:
+%   'Hidden', followed by an integer indicating the number of hidden
+%   variables.
+%   'MaxIter', followed by an integer indicating the number of max
+%   iterations.
+%   'Model', followed by a struct denoting a model to start with (see
+%   below).
+%  covariance options:
+%   'DiagQ0', if presented, will learn a diagonal covariance Q0
+%   'DiagQ', if presented, will learn a diagonal covariance Q
+%   'DiagR', if presented, will learn a diagonal covariance R
+%   'IsotropicQ0', if presented, will learn a diagonal and isotropic Q0
+%   'IsotropicQ', if presented, will learn a diagonal and isotropic Q
+%   'IsotropicR', if presented, will learn a diagonal and isotropic R
+%   'FullQ0', if presented, will learn any possible Q0
+%   'FullQ', if presented, will learn any possible Q
+%   'FullR', if presented, will learn any possible R
+%  Note these options could not coexist for the same covariance matrix.
+%  Default (no args given) the algorithm will learn with H=M, MaxIter=10,
+%  diagonal Q0, Q, R.
+%
+% Returns:
+%   model: a struct with the following attributes:
+%     A: transition matrix (also named as F in papers), H * H
+%     C: transmission  matrix(also called projection matrix, named as G
+% in papers), M * H
+%     Q: transition covariance, H * H 
+%     R: transmission covariance, M * M
+%     mu0: initial states (also named as z_0 sometimes), H * 1
+%     Q0: initial covariance, H * H
+%
+% derived from old function 
+% function [A, Gamma, C, Sigma, u0, V0, LL] = learn_kalman(x, H, maxIter)
+
+
+N = size(x, 2);
+M = size(x, 1);
+
+% get number of hidden variables
+a = find(strcmp('Hidden', varargin));
+if (isempty(a))
+  H = M;
+else
+  H = varargin{a+1};
+end
+
+% get the initial model
+a = find(strcmp('Model', varargin));
+if (isempty(a))
+  A = eye(H, H) + randn(H, H);
+  C = eye(M, H) + randn(M, H);
+  Q = eye(H, H);
+  R = eye(M, M);
+  mu0 = randn(H, 1);
+  Q0 = Q;
+else
+end
+m = mean(x);
+
+CONV_BOUND = 1e-5;
+
+ratio = 1;
+diff = 1;
+iter = 0;
+[u, V, P, oldLogli] = forward(x, A, Gamma, C, Sigma, u0, V0);
+
+while (ratio > CONV_BOUND || diff > CONV_BOUND) && (iter < maxIter)
+    iter = iter + 1;
+    [ucap, Vcap, J] = backward(u, V, P, A);
+    [A, Gamma, C, Sigma, u0, V0] = MLE_kalman(x, ucap, Vcap, J);
+    [u, V, P, logli] = forward(x, A, Gamma, C, Sigma, u0, V0);
+    logli = real(logli);
+    diff = (logli - oldLogli);
+    ratio = abs(diff/logli) ;
+    LL(iter) = logli;
+    oldLogli = logli;
+    fprintf('iteration = %d, logli = %d\n', iter, logli);
+end
