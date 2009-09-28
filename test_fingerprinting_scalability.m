aeaa3f079180d7e97915176f595dde468a12a9aa
leili <leili@03993aaa-8331-11dc-9d23-8cf24f0ad205>
2009-09-28 22:42:44 +0000

    add test stability test for fingerprint

---
+++ test_fingerprinting_scalability.m
@@ -0,0 +1,23 @@
+% test scalability of Kalman Finger Printing (PLF).
+clear;
+data = load('chlorine_level_data_cl2fullLarge.dat');
+
+X = data';
+N = size(X, 2);
+M = size(X, 1);
+candM = ceil(M ./ 5 .* [1:5]);
+H = 15;
+GapTick = ceil(N / 10);
+Trials = 10 : GapTick : N;
+time = zeros(length(candM), length(Trials));
+i = 1;
+for M = candM
+  j = 1;
+  for LEN = Trials
+    tic;
+    fingerprint(X(1:M, 1:LEN), 'Hidden', H, 'MaxIter', 20);
+    time(j, i) = toc;
+    i = i + 1;
+  end
+  j = j + 1;
+end
