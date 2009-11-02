ff220ddfe5f2c0d48dcdd3fe9a111ee987c993b5
leili <leili@03993aaa-8331-11dc-9d23-8cf24f0ad205>
2009-11-02 03:28:00 +0000

    abc

--- test_fingerprint_classify_simulation.m
+++ test_fingerprint_classify_simulation.m
@@ -67,9 +67,9 @@ c = [sin(t1) + 0.2 * sin(t2), cos(t1) + 0.2 * sin(t2 + pi/4)];
 
 X = [b c]';
 M = size(X, 1);
-figure;
+h = figure('position', [568   287   368   419]);
 for i = 1:M
-  subplot(M, 1, i);
+  subplot('position', [0.05, 1 - 0.15*i - 0.05 * i + 0.05, 0.92, 0.14]);
   hold on;
   ylim([-2, 2]);
   plot(X(i, :));
@@ -80,6 +80,51 @@ end
 
 class = [1 1 1 2 2];
 
-[group, entrop, P, D, mu0] = fingerprint_classify(X, 'Hidden', 6, 'MaxIter', 100, 'Class', class);
+[group, entrop, P, D, mu0, feature] = fingerprint_classify(X, 'Hidden', 6, 'MaxIter', 100, 'Class', class);
 
 periods = 2 * pi ./ angle(D);
+
+HIDDEN = length(D);
+
+ind = find(abs(imag(D)) > 1e-10); % assume 0==1e-10
+num_real = HIDDEN;
+% further eliminate the conjugate ones
+if (~ isempty(ind))
+  num_real = ind(1) - 1;
+  %D = D(subind);
+  
+  Q = abs(P(:, (num_real+1):2:end)); %only the magnitude
+end
+
+figure;
+imagesc(Q);
+colormap(gray);
+xa = xlim;
+line(xa, [1.5, 1.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [2.5, 2.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [3.5, 3.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [4.5, 4.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line([1.5, 1.5], [0.5, 5.5], 'LineWidth', 2, 'LineStyle', '--', 'Color', 'black');
+line([2.5, 2.5], [0.5, 5.5], 'LineWidth', 2, 'LineStyle', '--', 'Color', 'black');
+
+
+figure;
+imagesc(feature(:, 1:2));
+colormap(gray);
+xa = xlim;
+line(xa, [1.5, 1.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [2.5, 2.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [3.5, 3.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [4.5, 4.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line([1.5, 1.5], [0.5, 5.5], 'LineWidth', 2, 'LineStyle', '--', 'Color', 'black');
+
+[coeff, score] = princomp(X);
+figure;
+imagesc(score(:, 1:2));
+colormap(gray);
+xa = xlim;
+line(xa, [1.5, 1.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [2.5, 2.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [3.5, 3.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line(xa, [4.5, 4.5],'LineStyle', '--', 'Color', 'black', 'LineWidth', 2);
+line([1.5, 1.5], [0.5, 5.5], 'LineWidth', 2, 'LineStyle', '--', 'Color', 'black');
