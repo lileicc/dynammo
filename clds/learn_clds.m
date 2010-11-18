388abd776413d07025ee0920368d201c334ee951
leili <leili@03993aaa-8331-11dc-9d23-8cf24f0ad205>
2010-11-18 00:30:59 +0000

    added learn clds.m

---
+++ clds/learn_clds.m
@@ -0,0 +1,17 @@
+function [ model ] = learn_clds(X, varargin)
+%UNTITLED learning a complex dynamical systems
+%   Detailed explanation goes here
+% 
+% 
+% $Author: leili $@cs.cmu.edu
+% $Date: 2010-11-17 15:28:33 -0500 (Wed, 17 Nov 2010) $
+% $Rev: 300 $
+%
+
+
+
+
+
+
+end
+
